9aa0360aade259d45f937ceec0df89c5a286969a
vagrant <vagrant@kleer-csd-box>
2018-04-27 13:47:52 -0300

    Cuando lanzo el dado veo mi nueva posicion

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './lib/Tablero'
 get '/' do
     @dado = "Lanzar dado para iniciar"
     @posicionActual = 1
+    @posicionNueva = "ningun lado"
     erb:tablero
 end
   
@@ -12,5 +13,7 @@ end
 get '/Lanzar' do
     juego = Tablero.new
     @dado = juego.lanzarDado
+    @posicionActual = params['posicionActual'].to_i
+    @posicionNueva = "vas a:" + (@posicionActual.to_i + @dado.to_i).to_s
     erb:tablero
 end
--- features/tablero.feature
+++ features/tablero.feature
@@ -17,6 +17,11 @@ Scenario: Al lanzar el dado devuelve 3
 Scenario: Cuando inicia el juego veo mi posicion
 	  Given Visito la pagina principal
 	  Then Veo "Tu estas aqui: 1" 
+
+Scenario: Cuando estoy jugando y tiro el dado veo mi nueva posicion
+	  Given Visito la pagina principal
+	  When lanzo el dado
+	  Then Veo "vas a:"
 	
 
 	
